ae19d68baa928310b1dcf575bda5646ae0bf5d4d
Gonzalo Pierola <gpierola@vicomtech.org>
2022-12-02 09:03:42 +0100

    clean day 1 code

--- days/d1.ts
+++ days/d1.ts
@@ -4,6 +4,6 @@ import path from 'path';
 const data = fs.readFileSync( path.join(__dirname+'/inputs/d1.in'), 'utf-8')
 const elfs: number[] = (data.split('\r\n\r\n').map((e: string) => e.split('\r\n').reduce((acc: number, val: string) => acc+parseInt(val), 0))).sort((a: number, b: number) => b-a);
 
-console.log(elfs.at(0));
-console.log((elfs.at(0)||0)+(elfs.at(1)||0)+(elfs.at(2)||0));
+console.log(elfs[0]);
+console.log(elfs[0]+elfs[1]+elfs[2]);
 
