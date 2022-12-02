70f02530b05084a4c1013b87cd8f04fb6652ab55
Gonzalo Pierola <gpierola@vicomtech.org>
2022-12-02 09:00:44 +0100

    day 2

--- days/d1.ts
+++ days/d1.ts
@@ -1,8 +1,7 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 const data = fs.readFileSync( path.join(__dirname+'/inputs/d1.in'), 'utf-8')
-
 const elfs: number[] = (data.split('\r\n\r\n').map((e: string) => e.split('\r\n').reduce((acc: number, val: string) => acc+parseInt(val), 0))).sort((a: number, b: number) => b-a);
 
 console.log(elfs.at(0));
---
+++ days/d2.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+
+const result: any = {'A X': 3+1, 'B Y': 3+2, 'C Z': 3+3, 'A Y': 6+2, 'A Z': 0+3, 'B X': 0+1, 'B Z': 6+3, 'C X': 6+1, 'C Y': 0+2};
+const result2: any = {'A X': 0+3, 'B Y': 3+2, 'C Z': 6+1, 'A Y': 3+1, 'A Z': 6+2, 'B X': 0+1, 'B Z': 6+3, 'C X': 0+2, 'C Y': 3+3};
+
+const data = fs.readFileSync(path.join(__dirname, 'inputs/d2.in'), 'utf-8');
+
+const strategy: any[] = data.split(/[^A-Z]*\n[^A-Z]*/).map((match: string) => result[match]);
+const strategy2: any[] = data.split(/[^A-Z]*\n[^A-Z]*/).map((match: string) => result2[match]);
+
+console.log(strategy.reduce((acc, val) => acc+val, 0));
+console.log(strategy2.reduce((acc, val) => acc+val, 0));
